4913c952ce1fe064de648c7bf75dbe6f31819669
iammanshi <keshab@fusemachines.com>
2018-06-14 15:57:49 +0545

    added queries in schema file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,12 @@
 
+  INSERT INTO User (id, username, password, salary, age) VALUES 
+  (1, 'Alex123', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 3456, 33);
+  
+	create table if not exists oauth_access_token ( token_id VARCHAR(255), token VARBINARY(8000),
+ 	authentication_id VARCHAR(255) PRIMARY KEY, user_name VARCHAR(255), client_id VARCHAR(255),
+  	authentication VARBINARY(8000), refresh_token VARCHAR(255) );
+    
+	create table if not exists oauth_refresh_token (
+  	token_id VARCHAR(255),
+  	token VARBINARY(8000),
+ 	authentication VARBINARY(8000));
